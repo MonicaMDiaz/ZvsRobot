321c5632ea75bd37757ade27eea90261c6a2306c
MonicaMDiaz <124890620+MonicaMDiaz@users.noreply.github.com>
2023-03-13 16:30:24 -0500

    Initial commit

---
+++ Assets/RobotSphere/Assets/Scripts/RobotFreeAnim.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotFreeAnim : MonoBehaviour {
+
+	private CharacterController robot;
+	private float vel;
+	Vector3 rott;
+
+	Vector3 rot = Vector3.zero;
+	float rotSpeed = 40f;
+	Animator anim;
+
+	// Use this for initialization
+	void Awake()
+	{
+		anim = gameObject.GetComponent<Animator>();
+		gameObject.transform.eulerAngles = rot;
+
+		robot = GetComponent<CharacterController>();
+		vel = 20f;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		CheckKey();
+		gameObject.transform.eulerAngles = rot;
+		rott= new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+		if (rott.magnitude > 1)
+			rott = rott.normalized * vel;
+		else
+			rott = rott * vel;
+		robot.Move(rott * Time.deltaTime);
+
+	}
+
+	void CheckKey()
+	{
+		// Walk
+		if (Input.GetKey(KeyCode.W))
+		{
+			anim.SetBool("Walk_Anim", true);
+		}
+		else if (Input.GetKeyUp(KeyCode.W))
+		{
+			anim.SetBool("Walk_Anim", false);
+		}
+
+		// Rotate Left
+		if (Input.GetKey(KeyCode.Z))
+		{
+			rot[1] -= rotSpeed * Time.fixedDeltaTime;
+		}
+
+		// Rotate Right
+		if (Input.GetKey(KeyCode.X))
+		{
+			rot[1] += rotSpeed * Time.fixedDeltaTime;
+		}
+
+		// Roll
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			if (anim.GetBool("Roll_Anim"))
+			{
+				anim.SetBool("Roll_Anim", false);
+			}
+			else
+			{
+				anim.SetBool("Roll_Anim", true);
+			}
+		}
+
+		// Close
+		if (Input.GetKeyDown(KeyCode.LeftControl))
+		{
+			if (!anim.GetBool("Open_Anim"))
+			{
+				anim.SetBool("Open_Anim", true);
+			}
+			else
+			{
+				anim.SetBool("Open_Anim", false);
+			}
+		}
+	}
+
+}
